a75cd241d4c0f0d1aabe464775f66cf2e60d1237
Hannah Crawford <hannah@wolv.co>
2018-04-16 02:41:25 +0100

    Initial Commit.

---
+++ CharacterController2D.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+sealed public class CharacterController2D : MonoBehaviour {
+	// Properties
+	public float skinWidth = 0.01f;
+	public float minMoveDistance = 0.001f;
+
+	public float slopeLimit = 30f;
+	public Vector2 groundNormal {get; private set;}
+	private Vector2 maxSlopeNormal;
+
+	public LayerMask blockerMask;
+	public string oneWayPlatformTag = "One Way Platform";
+	
+	public bool ignoreOneWayPlatforms = false;
+	public bool ignoreOneWayPlatformsOnce = false;
+
+	// Flags
+	public bool isGrounded;
+	public bool isOnCeiling;
+	public bool isOnOneWayPlatform; 
+
+	// References
+	private Rigidbody2D rb;
+
+	// Misc
+	public ContactFilter2D contactFilter;
+
+	void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+		
+		contactFilter.useTriggers = false;
+		contactFilter.SetLayerMask(blockerMask);
+		contactFilter.useLayerMask = true;
+		
+		maxSlopeNormal = Quaternion.AngleAxis(slopeLimit, Vector3.forward) * Vector3.up;
+	}
+	
+	void Reset(){
+		rb = GetComponent<Rigidbody2D>();
+
+		rb.bodyType = RigidbodyType2D.Kinematic;
+		rb.useFullKinematicContacts = true;
+		rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+		rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+		rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+		blockerMask = Physics2D.GetLayerCollisionMask(gameObject.layer);
+	}
+
+	// Should be called in FixedUpdate.
+	public void Move(Vector2 moveVel, bool slideAlongGroundNormal = false) {
+		isGrounded = false;
+		isOnCeiling = false;
+		isOnOneWayPlatform = false;
+		
+		Vector2 xMoveVel = Vector2.Scale(moveVel, Vector2.right);
+		Vector2 yMoveVel = Vector2.Scale(moveVel, Vector2.up);
+		
+		if(slideAlongGroundNormal){
+			xMoveVel = new Vector2(groundNormal.y, -groundNormal.x) * moveVel.x;
+		}
+		
+		DoMovement(xMoveVel, true);	
+		DoMovement(yMoveVel, false);
+	}
+	
+	
+	private RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+	
+	private void DoMovement(Vector2 moveVel, bool xMovement = true) {
+		float distance = moveVel.magnitude;
+		if (distance > minMoveDistance) {
+			int count = rb.Cast(moveVel.normalized, contactFilter, hitBuffer, distance + skinWidth);
+	
+			for (int i = 0; i < count; i++) {
+				bool oneWayPlatform = hitBuffer[i].transform.tag == oneWayPlatformTag;
+				
+				Vector2 currentNormal = hitBuffer[i].normal;
+				
+				if(!oneWayPlatform){
+					if (currentNormal.y >= maxSlopeNormal.y) {
+						isGrounded = true;
+						groundNormal = currentNormal;
+					} else if (currentNormal.y <= -maxSlopeNormal.y) {
+						isOnCeiling = true;
+					}
+
+					float modifiedDistance = hitBuffer[i].distance - skinWidth;
+					distance = modifiedDistance < distance ? modifiedDistance : distance;
+				}else{
+					if(ignoreOneWayPlatforms || ignoreOneWayPlatformsOnce){
+						ignoreOneWayPlatformsOnce = false;
+						continue;
+					}
+					
+					if (currentNormal.y >= maxSlopeNormal.y && moveVel.y <= 0 && hitBuffer[0].distance > 0) {
+						isGrounded = true;
+						groundNormal = currentNormal;
+					}
+					
+					if(isGrounded && moveVel.x == 0){
+						isOnOneWayPlatform = true;
+						float modifiedDistance = hitBuffer[i].distance - skinWidth;
+						distance = modifiedDistance < distance ? modifiedDistance : distance;
+					}
+				}
+			}
+		}
+
+		rb.position = rb.position + moveVel.normalized * distance;
+	}
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+# CharacterController2D
+
+CharacterController2D is a reimplementation of Unity's CharacterController class using the 2D physics engine that doesnt share the same limitations of other solutions.
+
+## Features
+
+- Behaves almost exactly like CharacterController.
+
+- Not raycast-based, uses Rigidbody2D.Cast() therefore takes into account the exact shape of your character's collider.
+
+- Only uses 2 calls to Rigidbody2D.Cast() per Move() call so is highly performant.
+
+- One way platforms with any collider type (BoxCollider, PolygonCollider, etc...) using only a tag.
+
+- Supports ignoring one way platforms for a single frame or extended periods (useful for dropping through platforms).
+
+- Supports "sliding" along the ground normal for smooth slope traversial.
+
+- Slope limit for ground and ceiling.
+
+- Blocker mask allows you to set what layers affect movement.
+
+## Setup
+
+Add a CharacterController2D to your player object and it will add a Rigidbody2D component and configure it to it's optimal defaults.
+
+You will need to add a collider to your player. It is recommended you use a BoxCollider or PolygonCollider since if you have slopes in your collider that are steeper than slopeLimit then you may have issues with isGrounded and sliding along the ground normal.
+
+Here is good example of an ideal collider setup (slope limit 30).
+
+![Example Screenshot](https://i.imgur.com/1XAE9Cs.png)
+
+## Usage
+
+You can use CharacterController2D the same way you would use a CharacterController with a few additional features and the omission of SimpleMove().
+
+In addition to isGrounded, CharacterController2D provides a few other useful values.
+
+- isOnCeiling
+- isOnOneWayPlatform
+- groundNormal
+
+## Licence
+
+[The Unlicence](http://unlicense.org/), you can use CharacterController2D in anyway you see fit however I would love to hear from you if you use it in your project!
